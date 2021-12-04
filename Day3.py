552954a4fe43b3ba964dafa5d6c3379aed978748
Adam Cosentino <acosentino326@gmail.com>
2021-12-03 19:40:20 -0500

    Day 3 complete

--- Day3.py
+++ Day3.py
@@ -3,18 +3,76 @@ class Day:
 
     def __init__(self, input_array=None):
         self.day_input = input_array
+        # self.day_input = ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]
 
     def __is_valid(self):
         return self.day_input is not None
 
     def part1(self):
-        if self.__is_valid():
+        if not self.__is_valid():
             return
 
-        print("Part 1: Not Complete")
+        epsilon = ""
+        gamma = ""
+        commonality = []
+        for line in self.day_input:
+            for i in range(len(line)):
+                if len(commonality) < i + 1:
+                    commonality.append(0)
+                match line[i]:
+                    case "0":
+                        commonality[i] -= 1
+                    case "1":
+                        commonality[i] += 1
+
+        for bit in commonality:
+            if bit > 0:
+                epsilon += "1"
+                gamma += "0"
+            else:
+                epsilon += "0"
+                gamma += "1"
+
+        epsilon_value = int(epsilon, 2)
+        gamma_value = int(gamma, 2)
+
+        # print("Epsilon: " + str(epsilon) + " = " + str(epsilon_value))
+        # print("Gamma: " + str(gamma) + " = " + str(gamma_value))
+        print("Part 1: " + str(epsilon_value * gamma_value))
 
     def part2(self):
-        if self.__is_valid():
+        if not self.__is_valid():
             return
 
-        print("Part 2: Not Complete")
+        oxygen = self.get_rating(self.day_input, 0, "O")
+        oxygen_value = int(oxygen, 2)
+        scrubber = self.get_rating(self.day_input, 0, "CO2")
+        scrubber_value = int(scrubber, 2)
+
+        # print("Oxygen Generator Rating: " + str(oxygen) + " = " + str(oxygen_value))
+        # print("CO2 Scrubber Rating: " + str(scrubber) + " = " + str(scrubber_value))
+        print("Part 2: " + str(oxygen_value * scrubber_value))
+
+    def get_rating(self, array, index, rating):
+        if len(array) == 1:
+            return array[0]
+
+        reduced_array = []
+        common = 0
+
+        for line in array:
+            match line[index]:
+                case "0":
+                    common -= 1
+                case "1":
+                    common += 1
+
+        match = "0"
+        if common >= 0 and rating == "O" or common < 0 and rating == "CO2":
+            match = "1"
+
+        for line in array:
+            if line[index] == match:
+                reduced_array.append(line)
+
+        return self.get_rating(reduced_array, index + 1, rating)
